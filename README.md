522922beba3e68a769dee8b29977cd5230c5a6b3
Yuekai Jia <equation618@gmail.com>
2022-05-08 18:56:46 +0800

    Update benchmark.sh and README.md

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # YCSB-C-SGX
 
+SQLite (v3.19.3) database inside Intel SGX enclave, integrated with a C++ version of [Yahoo! Cloud Serving Benchmark](https://github.com/brianfrankcooper/YCSB/wiki).
+
+Reference: 
+
+1. https://github.com/yerzhan7/SGX_SQLite
+2. https://github.com/basicthinker/YCSB-C
+
 ## Build for native
 
 ```
@@ -17,4 +24,21 @@ make SGX_MODE=HW SGX_DEBUG=0 SGX_PRERELEASE=1
 
 ```
 ./app -db sqlite -P YCSB-C/workloads/workloada.spec
+```
+
+## Benchmark
+
+Benchmark the transaction throughput using the YCSB workloads as the number of records increase from 5K to 200K.
+
+```
+./benchmark.sh [OPTIONS] <TAG>
+
+Options:
+    -h                      Display this message
+    -r <NUMBER>             Number of runs (default: 10)
+    -w <WORKLOAD>           Workload ID in A, B, C, D, E, F (default: A)
+    -t <OP_COUNT>           Number of operations (default: 100000)
+
+Arguments:
+    <TAG>                   Tag of this evalutaion
 ```
--- benchmark.sh
+++ benchmark.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
 function usage() {
-    echo "Usage: $0 [OPTIONS] <APP> <TAG>"
+    echo "Usage: $0 [OPTIONS] <TAG>"
     echo ""
     echo "Options:"
     echo "    -h                      Display this message"
     echo "    -r <NUMBER>             Number of runs (default: 10)"
+    echo "    -w <WORKLOAD>           Workload ID in A, B, C, D, E, F (default: A)"
     echo "    -t <OP_COUNT>           Number of operations (default: 100000)"
     echo ""
     echo "Arguments:"
-    echo "    <APP>                   Executable file"
     echo "    <TAG>                   Tag of this evalutaion"
     echo ""
 }
 
-while getopts "r:t:h" opt
+while getopts "r:t:w:h" opt
 do
    case "$opt" in
       r) runs="$OPTARG" ;;
       t) op_count="$OPTARG" ;;
-      h | ?)
-        usage
-        exit 0
-        ;;
+      w) workload="$OPTARG" ;;
+      h | ?) usage; exit 0 ;;
    esac
 done
 
 shift $((OPTIND - 1))
-if [ -z "$1" -o -z "$2" ]; then
+if [ -z "$1" ]; then
     usage
     exit 0
 fi
@@ -40,10 +38,27 @@ if [ -z "$op_count" ]; then
     op_count=100000
 fi
 
-app=$1
-res_dir=result-$2-$(date +%y%m%d-%H%M%S)
+if [ -z "$APP" ]; then
+    APP=./app
+fi
+
+if [ -z "$workload" ]; then
+    workload=A
+fi
+
+case $workload in
+    a | A) workload=workloada ;;
+    b | B) workload=workloadb ;; 
+    c | C) workload=workloadc ;;
+    d | D) workload=workloadd ;;
+    e | E) workload=workloade ;;
+    f | F) workload=workloadf ;;
+    *) usage; exit 0 ;;
+esac
+
+res_dir=result-$1-$(date +%y%m%d-%H%M%S)
 rec_counts="5 10 20 40 60 80 100 120 140 160 180 200"
-workload=./YCSB-C/workloads/workloada.spec
+workload=./YCSB-C/workloads/$workload.spec
 
 declare -A all_tput
 
@@ -56,7 +71,7 @@ do
         rec_count=$(expr $n \* 1000)
         resfile=$res_dir/${rec_count}_$i.res
 
-        $app -db sqlite -P $workload -n $rec_count -t $op_count > $resfile || exit $?
+        $APP -db sqlite -P $workload -n $rec_count -t $op_count > $resfile || exit $?
         
         tput=$(tail -n1 $resfile)
         all_tput[$n]="${all_tput[$n]} $tput"
